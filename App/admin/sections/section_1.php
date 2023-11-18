1b2258c3460bda063d35bf02bcb30a537ea51b8c
fer-gc05 <gilcorrealuis09@gmail.com>
2023-11-18 18:57:51 -0500

    commit

--- App/admin/sections/section_1.php
+++ App/admin/sections/section_1.php
@@ -53,7 +53,7 @@
                     <div class="card-body d-flex">
                         <div>
                             <div class="circle rounded-circle bg-info align-self-center d-flex mr-3">
-                            <i class='bx bx-user-circle text-secundary align-self-center mx-auto lead'></i>
+                                <i class='bx bx-user-circle text-secundary align-self-center mx-auto lead'></i>
                             </div>
                         </div>
                         <div class="align-self-center">
@@ -66,32 +66,28 @@
         </div>
 
         <div class="row mb-3">
-            <div class="col-xl-7 col-lg-12">
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th colspan="co1"><small class="font-weight-bold">Producto<small></th>
-                                <th scope="col"><small class="font-weight-bold">Cantidad<small></th>
-                                <th scope="col"><small class="font-weight-bold">Precio<small></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="shadow-sm">
-                                <td><span>Producto 1</span></td>
-                                <td><span>100</span>
-                                </td>
-                                <td class="align-middle"><span class="badge badge-info text-black">$100.000</span></td>
-                            </tr>
 
-                        </tbody>
-                    </table>
-                </div>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th colspan="co1"><small class="font-weight-bold">Producto<small></th>
+                            <th scope="col"><small class="font-weight-bold">Cantidad<small></th>
+                            <th scope="col"><small class="font-weight-bold">Precio<small></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="shadow-sm">
+                            <td><span>Producto 1</span></td>
+                            <td><span>100</span>
+                            </td>
+                            <td class="align-middle"><span class="badge badge-info text-black">$100.000</span></td>
+                        </tr>
+
+                    </tbody>
+                </table>
             </div>
-
         </div>
-
-
     </section>
 </div>
 
