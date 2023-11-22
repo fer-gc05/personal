a6917d38deb18e98dfa5a2f57aa8e0f6af9ade24
fer-gc05 <gilcorrealuis09@gmail.com>
2023-11-21 22:13:43 -0500

    commit

--- App/admin/sections/section_2.php
+++ App/admin/sections/section_2.php
@@ -1,27 +1,26 @@
 <?php include ("../templates/header.php");?>
 <div id="content" class="container-fluid p-5">
-<div class="btn-group dropright">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-    Tablas
-  </button>
-  <div class="dropdown-menu">
-    <button class="dropdown-item" id="B-cl" type="button">Cliente</button>
-    <button class="dropdown-item" id="B-o" type="button">Compra</button>
-    <button class="dropdown-item" id="B-e" type="button">Empleado</button>
-    <button class="dropdown-item" id="B-v" type="button">Venta</button>
-    <button class="dropdown-item" id="B-ve" type="button">Vende</button>
-    <button class="dropdown-item" id="B-p" type="button">Producto</button>
-    <button class="dropdown-item" id="B-pr" type="button">Proveedor</button>
-    <button class="dropdown-item" id="B-s" type="button">Suministra</button>
-  </div>
-</div>
-<section class="py-3">
-<div class="row mb-3">
-            <div class="table-responsive" id= "asynchronous">
-                
+    <div class="btn-group dropright">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown"
+            aria-expanded="false">Tablas</button>
+        <div class="dropdown-menu">
+            <button class="dropdown-item" id="B-cl" type="button">Cliente</button>
+            <button class="dropdown-item" id="B-o" type="button">Compra</button>
+            <button class="dropdown-item" id="B-e" type="button">Empleado</button>
+            <button class="dropdown-item" id="B-v" type="button">Venta</button>
+            <button class="dropdown-item" id="B-ve" type="button">Vende</button>
+            <button class="dropdown-item" id="B-p" type="button">Producto</button>
+            <button class="dropdown-item" id="B-pr" type="button">Proveedor</button>
+            <button class="dropdown-item" id="B-s" type="button">Suministra</button>
+        </div>
+    </div>
+    <section class="py-3">
+        <div class="row mb-3">
+            <div class="table-responsive" id="asynchronous">
+
             </div>
         </div>
 
-</section>
+    </section>
 </div>
 <?php include ("../templates/footer.php");?>
