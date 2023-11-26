19ac8fa6ed5e76add6751d4235dc5835065e6306
fer-gc05 <gilcorrealuis09@gmail.com>
2023-11-26 09:07:23 -0500

    commit

--- App/admin/config/bd/clients.php
+++ App/admin/config/bd/clients.php
@@ -6,32 +6,35 @@ $conexion = $objeto->Conectar();
 $_POST = json_decode(file_get_contents("php://input"), true);
 $opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
 $Id = (isset($_POST['Id'])) ? $_POST['Id'] : '';
-$Cedula = (isset($_POST['Cedula'])) ? $_POST['Cedula']:'';
+$Cedula = (isset($_POST['Cedula'])) ? $_POST['Cedula'] : '';
 $Nombre = (isset($_POST['Nombre'])) ? $_POST['Nombre'] : '';
 
-switch($opcion){
+switch ($opcion) {
     case 1:
-        $consulta = "INSERT INTO Clientes ( Id, Cedula, Nombre) VALUES('$Id', '$Cedula', $Nombre') ";	
+        $consulta = "INSERT INTO Clientes (Cedula, Nombre) VALUES('$Cedula', '$Nombre') ";
         $resultado = $conexion->prepare($consulta);
-        $resultado->execute();                
+        $resultado->execute();
         break;
     case 2:
-        $consulta = "UPDATE Clientes SET Cedula='$Cedula, Nombre='$Nombre'' WHERE Id='$Id'";		
+        $consulta = "UPDATE Clientes SET Cedula='$Cedula', Nombre='$Nombre' WHERE Id='$Id'";
         $resultado = $conexion->prepare($consulta);
-        $resultado->execute();                        
-        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
-        break;        
+        $resultado->execute();
+        break;
     case 3:
-        $consulta = "DELETE FROM Clientes WHERE Id='$Id' ";		
+        $consulta = "DELETE FROM Clientes WHERE Id='$Id' ";
         $resultado = $conexion->prepare($consulta);
-        $resultado->execute();                           
-        break;         
+        $resultado->execute();
+        break;
     case 4:
         $consulta = "SELECT * FROM Clientes";
         $resultado = $conexion->prepare($consulta);
         $resultado->execute();
-        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
         break;
 }
-print json_encode($data, JSON_UNESCAPED_UNICODE);
+
+
+echo json_encode($data, JSON_UNESCAPED_UNICODE);
+
 $conexion = NULL;
+?>
--- App/admin/templates/clients-template.php
+++ App/admin/templates/clients-template.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda M&M</title>
     <link href="../../libreries/bootstrap/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css" rel="stylesheet">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="../../libreries/sweetalert2/sweetalert2.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -19,18 +19,20 @@
                         <tr>
                             <th>Cedula</th>
                             <th>Nombre</th>
+                            <th>Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="shadow-sm" v-for="(cliente,indice) of clientes">
-                            <td>{{cliente.Cedula}}</td>
-                            <td>{{cliente.Nombre}}</td>
+                        <tr class="shadow-sm" v-for="(cliente, indice) of clientes">
+                            <td>{{ cliente.Cedula }}</td>
+                            <td>{{ cliente.Nombre }}</td>
                             <td>
                                 <div class="btn-group" role="group">
-                                    <button class="btn btn-secondary" title="Editar">
+                                    <button @click="btnEditar(cliente.Id, cliente.Cedula, cliente.Nombre)"
+                                        class="btn btn-secondary" title="Editar">
                                         <i class='bx bx-edit-alt'></i>
                                     </button>
-                                    <button class="btn btn-danger" title="Eliminar">
+                                    <button @click="btnEliminar(cliente.Id)" class="btn btn-danger" title="Eliminar">
                                         <i class='bx bx-trash-alt'></i>
                                     </button>
                                 </div>
@@ -39,16 +41,18 @@
                     </tbody>
                 </table>
                 <div class="col">
-                    <button class="btn btn-success" title="Nuevo"><i class='bx bxs-save'></i></button>
+                    <button @click="btnInsertar" class="btn btn-success" title="Nuevo"><i
+                            class='bx bxs-save'></i></button>
                 </div>
             </div>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="../../libreries/sweetalert2/sweetalert2.all.min.js"></script>
+    <script src="../../libreries/Popper/popper.min.js"></script>
+    <script src="../../libreries/Vue/vue.js"></script>
     <script src="../../libreries/jquery/jquery-3.3.1.min.js"></script>
+    <script src="../../libreries/Axios/axios.js"></script>
     <script src="../../libreries/bootstrap/bootstrap.min.js"></script>
 
     <script>
@@ -63,33 +67,136 @@
         },
         methods: {
             mostrar: function() {
-                $.ajax({
-                    url: url,
-                    type: 'POST',
-                    data: {
-                        opcion: 4
-                    },
-                    success: function(response) {
-                        try {
-                            app.clientes = JSON.parse(response);
-                            console.log('Datos obtenidos:', app.clientes);
-                        } catch (error) {
-                            console.error('Error al parsear la respuesta JSON:', error);
-                        }
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        console.error('Error en la solicitud Ajax:', textStatus, errorThrown);
-                    }
+                axios.post(url, {
+                    opcion: 4
+                }).then(response => {
+                    this.clientes = response.data;
+                    console.log(this.clientes);
                 });
             },
-            // ... (resto de tus métodos CRUD)
+
+            insertar: function() {
+                axios.post(url, {
+                    opcion: 1,
+                    Cedula: this.Cedula,
+                    Nombre: this.Nombre
+                }).then(response => {
+                    this.mostrar();
+                });
+
+                this.Cedula = "";
+                this.Nombre = "";
+            },
+
+            editar: function(Id, Cedula, Nombre) {
+                axios.post(url, {
+                    opcion: 2,
+                    Id: Id,
+                    Cedula: Cedula,
+                    Nombre: Nombre
+                }).then(response => {
+                    this.mostrar();
+                });
+            },
+
+            eliminar: function(Id) {
+                axios.post(url, {
+                    opcion: 3,
+                    Id: Id
+                }).then(response => {
+                    this.mostrar();
+                });
+            },
+
+            btnInsertar: async function() {
+                const {
+                    value: formValues
+                } = await Swal.fire({
+                    title: 'NUEVO',
+                    html: '<div class="row"><label class="col-sm-3 col-form-label">Cedula del cliente</label><div class="col-sm-7"><input id="cedula" type="text" class="form-control" /></div></div><div class="row"><label class="col-sm-3 col-form-label">Nombre del cliente</label><div class="col-sm-7"><input id="nombre" type="text" class="form-control" /></div></div>',
+                    focusConfirm: false,
+                    showCancelButton: true,
+                    confirmButtonText: 'Guardar',
+                    confirmButtonColor: '#1cc88a',
+                    cancelButtonColor: '#3085d6',
+                    preConfirm: () => {
+                        return [
+                            this.Cedula = document.getElementById('cedula').value,
+                            this.Nombre = document.getElementById('nombre').value
+                        ];
+                    }
+                });
+
+                if (this.Cedula === "" || this.Nombre === "") {
+                    Swal.fire({
+                        type: 'info',
+                        title: 'Datos incompletos',
+                    });
+                } else {
+                    this.insertar();
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                    Toast.fire({
+                        type: 'success',
+                        title: '¡Datos agregados!'
+                    });
+                }
+            },
+
+            btnEditar: async function(Id, Cedula, Nombre) {
+                await Swal.fire({
+                    title: 'EDITAR',
+                    html: '<div class="form-group"><div class="row"><label class="col-sm-3 col-form-label">Cedula</label><div class="col-sm-7"><input id="cedula" value="' +
+                        Cedula +
+                        '" type="text" class="form-control"></div></div><div class="row"><label class="col-sm-3 col-form-label">Nombre</label><div class="col-sm-7"><input id="nombre" value="' +
+                        Nombre +
+                        '" type="text" class="form-control"></div></div>',
+                    focusConfirm: false,
+                    showCancelButton: true,
+                }).then((result) => {
+                    if (result.value) {
+                        Cedula = document.getElementById('cedula').value,
+                            Nombre = document.getElementById('nombre').value,
+                            
+                            this.editar(Id, Cedula, Nombre);
+                        Swal.fire(
+                            '¡Actualizado!',
+                            'El registro ha sido actualizado.',
+                            'success'
+                        )
+                    }
+                });
+
+            },
+
+            btnEliminar: async function(Id) {
+                Swal.fire({
+                    title: '¿Está seguro de borrar este registro?',
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#3085d6',
+                    confirmButtonText: 'Eliminar'
+                }).then((result) => {
+                    if (result.value) {
+                        this.eliminar(Id);
+                        Swal.fire(
+                            '¡Eliminado!',
+                            'El registro ha sido borrado.',
+                            'success'
+                        );
+                    }
+                });
+            }
         },
         created: function() {
             this.mostrar();
         },
-        computed: {
-            // ...
-        }
+        computed: {}
     });
     </script>
 
